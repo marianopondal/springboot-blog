780b4649262dac2a8ca2f98036213a938d0463c0
Mariano Pondal <marianopondal@gmail.com>
2022-06-16 10:53:36 -0300

    Adding feature DELETE /api/posts/{id}

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -52,6 +52,11 @@ public class PostServiceImpl implements PostService {
         return mapToDto(updatedPost);
     }
 
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
 
     private PostDto mapToDto(Post post) {
         PostDto postDto = new PostDto();
