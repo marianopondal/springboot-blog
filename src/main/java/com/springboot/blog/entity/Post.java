5165aef1d678e6aa05b38be448576c00998f210d
Mariano Pondal <marianopondal@gmail.com>
2022-06-17 10:44:09 -0300

    Adding collection of comments into posts on requests:      - GET /api/posts      - GET /api/posts/{id}

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -1,15 +1,14 @@
 package com.springboot.blog.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @NoArgsConstructor
-@Data
 @AllArgsConstructor
 
 @Entity
